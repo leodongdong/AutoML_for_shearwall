5cf4c936d362a0a5c7048be455728d02cba0cbe4
leodongdong <109461250+leodongdong@users.noreply.github.com>
2023-03-17 13:38:03 +0800

    Update automl_ALE.py

--- automl_ALE.py
+++ automl_ALE.py
@@ -52,8 +52,8 @@ class_names = le.classes_
 data = data[1:,2:17]
 print (data)
 
+# data processing
 categorical_features = [12,13,14]
-
 categorical_names = {}
 for feature in categorical_features:
     le = sklearn.preprocessing.LabelEncoder()
@@ -61,13 +61,11 @@ for feature in categorical_features:
     data[:, feature] = le.transform(data[:, feature])
     categorical_names[feature] = le.classes_
 print (data)
-
 data = data.astype(float)
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
-
 transformer = ColumnTransformer(
     transformers=[
         ("OneHot",        # Just a name
@@ -77,40 +75,32 @@ transformer = ColumnTransformer(
     ],
     remainder='passthrough' # donot apply anything to the remaining columns
 )
-
 transformer.fit(data)
-print (data.shape)
 encoded_train = transformer.transform(data)
-print (encoded_train.shape)
 
 
+# AutoML training
 lr = autosklearn.classification.AutoSklearnClassifier(
         time_left_for_this_task = max_seconds,
         per_run_time_limit = max_seconds // 10,
         metric = autosklearn.metrics.f1,
 )
-
-
 lr.fit(data, labels, dataset_name='shearwall')
 
+#ALE analysis
 proba_fun_lr = lr.predict_proba
-
 proba_ale_lr = ALE(proba_fun_lr, feature_names=feature_names, target_names=target_names)
-
 proba_exp_lr = proba_ale_lr.explain(data)
-
 alevalues = proba_exp_lr["ale_values"]
 ale0 = proba_exp_lr["ale0"]
 feature_values = proba_exp_lr["feature_values"]
-
 alevalues= pd.DataFrame(alevalues)
 ale0= pd.DataFrame(ale0)
 feature_values= pd.DataFrame(feature_values)
 
+# ALE analysis result save
 alevalues.to_excel(r'alevalues1.xlsx', sheet_name='train', index = False)
 ale0.to_excel(r'ale01.xlsx', sheet_name='train', index = False)
 feature_values.to_excel(r'feature_values1.xlsx', sheet_name='train', index = False)
-
 plot_ale(proba_exp_lr, n_cols=2, fig_kw={'figwidth': 80, 'figheight': 50}, sharey=None)
-
 plt.savefig('picture1.jpg')
